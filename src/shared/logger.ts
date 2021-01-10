7305c8d35f399a264dc839783ae35e07505f30c4
nokazn <nokazn@gmail.com>
2021-01-11 00:28:01 +0900

    configure log settings

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -6,8 +6,28 @@
 
 import log4js from 'log4js';
 
-// log4js.configure({})
-
-const logger = log4js.getLogger();
+log4js.configure({
+  appenders: {
+    stdout: {
+      type: 'stdout',
+    },
+    prod: {
+      type: 'file',
+      filename: 'server.log',
+      backups: 1,
+    },
+  },
+  categories: {
+    default: {
+      appenders: ['stdout'],
+      level: 'all',
+    },
+    prod: {
+      appenders: ['stdout', 'prod'],
+      level: 'debug',
+    },
+  },
+});
+const logger = log4js.getLogger(process.env.NODE_ENV === 'production' ? 'prod' : 'default');
 
 export default logger;
