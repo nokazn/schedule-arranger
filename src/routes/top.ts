b16a4a8b847df15c09df4fd699a5e698ca4ec900
nokazn <nokazn@gmail.com>
2021-01-29 22:10:00 +0900

    format date in top page

--- src/routes/top.ts
+++ src/routes/top.ts
@@ -3,10 +3,18 @@ import httpStatusCodes from 'http-status-codes';
 import createErrors from 'http-errors';
 
 import { ScheduleDao } from '~/daos';
+import dayjs from '~/shared/dayjs';
 import logger from '~/shared/logger';
+import type { ScheduleAttributes } from '~/entities';
 
 const router = Router();
 const { INTERNAL_SERVER_ERROR } = httpStatusCodes;
+
+type RenderOptions = {
+  title: string;
+  user: Express.User | undefined;
+  schedules: (ScheduleAttributes & { formattedUpdatedAt: string })[];
+};
 
 router.get('/', (req, res, next) => {
   logger.info(req.user);
@@ -25,10 +33,13 @@ router.get('/', (req, res, next) => {
     order: [['"updatedAt', 'DESC']],
   })
     .then((schedules) => {
-      res.render('top', {
+      res.render<RenderOptions>('top', {
         title: '予定調整くん',
         user: req.user,
-        schedules,
+        schedules: schedules.map((s) => ({
+          ...s,
+          formattedUpdatedAt: dayjs(s.updatedAt).format('YYYY/MM/DD HH:mm'),
+        })),
       });
     })
     .catch((err: Error) => {
